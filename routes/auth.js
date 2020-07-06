3fcc23e3b55e937c02a1c59687a54745db5bc04f
Marcus Jensen <marcusjensen1515@gmail.com>
2020-07-06 15:16:30 -0700

    needed to update Joi to use the updated syntax

--- routes/auth.js
+++ routes/auth.js
@@ -5,29 +5,33 @@ const User = require('../model/User');
 
 const Joi = require('@hapi/joi');
 
-const schema = {
+const schema = Joi.object({
   name: Joi.string().min(6).required(),
   email: Joi.string().min(6).required().email(),
   password: Joi.string().min(6).required()
-};
-
-// Validating data before we create a user
+});
 
 
 
 
 router.post('/register', async (req,res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  try{
-    const savedUser = await user.save();
-    res.send(savedUser);
-  }catch(err){
-    res.status(400).send(err);
-  }
+
+  // Validating data before we create a user
+
+  const validation = schema.validate(req.body);
+  res.send(validation);
+
+  // const user = new User({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  // });
+  // try{
+  //   const savedUser = await user.save();
+  //   res.send(savedUser);
+  // }catch(err){
+  //   res.status(400).send(err);
+  // }
 });
 
 
