591c3fb851cbdb725d17e14fc65f71235b879307
Marcus Jensen <marcusjensen1515@gmail.com>
2020-07-06 15:03:17 -0700

    adding validation middlware through Joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+//VALIDATION
+
+const Joi = require('@hapi/joi');
+
+const schema = {
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required()
+};
+
+
 router.post('/register', async (req,res) => {
   const user = new User({
     name: req.body.name,
