b00eb79705bc529b5a2c1c9cf703141d42895147
Marcus Jensen <marcusjensen1515@gmail.com>
2020-07-07 09:47:59 -0700

    add error if email already exists. functioning correctly

--- routes/auth.js
+++ routes/auth.js
@@ -2,17 +2,19 @@ const router = require('express').Router();
 const User = require('../model/User');
 const {registerValidation, loginValidation} = require('../validation')
 
-
-
-
-
-
 router.post('/register', async (req,res) => {
 
   // Validating data before we create a user
   const error = registerValidation(req.body);
 
   if(error.error) return res.status(400).send(error.error.details[0].message);
+
+  //Check if user is already in the database
+
+  const emailExists = await User.findOne({email: req.body.email});
+  if(emailExists) return res.status(400).send('this email already exists');
+
+  //Creating new user
 
   const user = new User({
     name: req.body.name,
