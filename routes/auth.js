89323f15b06eba4f9cc5fa6622a69e6c59a429eb
Marcus Jensen <marcusjensen1515@gmail.com>
2020-07-07 09:33:49 -0700

    correctly sending error back if there is one. Processing requrest if there is no error

--- routes/auth.js
+++ routes/auth.js
@@ -19,19 +19,20 @@ router.post('/register', async (req,res) => {
   // Validating data before we create a user
 
   const error = schema.validate(req.body);
-  res.send(error.error.details[0].message);
 
-  // const user = new User({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password,
-  // });
-  // try{
-  //   const savedUser = await user.save();
-  //   res.send(savedUser);
-  // }catch(err){
-  //   res.status(400).send(err);
-  // }
+  if(error.error) return res.status(400).send(error.error.details[0].message);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try{
+    const savedUser = await user.save();
+    res.send(savedUser);
+  }catch(err){
+    res.status(400).send(err);
+  }
 });
 
 
