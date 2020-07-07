e7d42c1f41bd9455136c5480d85df80e92d6b22a
Marcus Jensen <marcusjensen1515@gmail.com>
2020-07-07 13:15:16 -0700

    able to get dummy post. as of now it is not private

---
+++ routes/posts.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+router.get('/', (req,res) => {
+  res.json({posts: {title: 'my first post', description: 'random data non users cant access'
+    }
+  });
+});
+
+
+module.exports = router;
